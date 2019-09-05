298921a3ff93837fb8c00a2e0471b6e1fa625f7d
thinct <thinct_123@foxmail.com>
2019-09-05 14:12:28 +0800

    强制关闭已经打开并且为退出的进程

--- DevAssist.py
+++ DevAssist.py
@@ -1,9 +1,20 @@
 import shutil
 import tkinter.messagebox
+import os  
 
 try:
+    if 'Calibration_HandEye' in os.popen('tasklist /FI "IMAGENAME eq Calibration_HandEye.exe"').read():
+        os.system('TASKKILL /F /IM Calibration_HandEye.exe')
+    if 'Calibration_HandEye' in os.popen('tasklist /FI "IMAGENAME eq Calibration_HandEye.exe"').read():
+        os.system('TASKKILL /F /IM Calibration_HandEye.exe')
+    if 'Calibration_HandEye' in os.popen('tasklist /FI "IMAGENAME eq Calibration_HandEye.exe"').read():
+        os.system('TASKKILL /F /IM Calibration_HandEye.exe')
+        
     path = shutil.copy(r'D:\WorkCode\QtTempCodes\Calibration_HandEye\release\Calibration_HandEye.exe'\
     , r'D:\Others\DengJin\SMTMainWindow_20190828_x64\Calibration_HandEye.exe')
+    
+    os.system(r'D:\Others\DengJin\SMTMainWindow_20190828_x64\Calibration_HandEye.exe')
+    
 except:
     tkinter.Tk().withdraw();
     tkinter.messagebox.showerror('错误','出错了')
